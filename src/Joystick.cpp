7448984fd0c95a7c1f7e412a975a260738a4643d
RoCorbera <rocorbera@gmail.com>
2020-05-16 00:06:38 -0300

    New version 1.0 - works for STM32 & Roger's Cores

--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -2,19 +2,27 @@
    BlueVGA library - VGA Driver Library for STM32F103
 
    - This library is intended to work in Arduino IDE using Bluepill STM32F103C8 or STM32F103CB boards
-   - It uses ARDUINO Roger's core for STM32duino board. Please check it at https://github.com/rogerclarkmelbourne/Arduino_STM32
-     you will find arduino installation for Roger's core at https://github.com/rogerclarkmelbourne/Arduino_STM32/wiki/Installation
-
+   - It works on both STM32 Core and Roger's core for STM32F103C board.
    - It was tested and runs using the following Arduino Settings for the board:
        Generic STM32F103C series
-       Optimize Os (Smallest) or O1 (Fast)
+       Optimize Os (Smallest)
        Variant STM32F103C8 or STM32F103CB
        CPU Speed(MHz) 72MHz (Normal)
 
-    Created by Rodrigo Corbera (rocorbera@gmail.com)
+    Author Rodrigo Patricio Garcia Corbera (rocorbera@gmail.com)
+    Copyright © 2017-2020 Rodrigo Patricio Garcia Corbera. 
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
     This code is licensed as Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) - https://creativecommons.org/licenses/by-nc-sa/4.0/
-    Full legal language of the license here granted can be found at https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+    Redistributions of source code must retain the above copyright notice, and meet al conditions as defined in  https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the this disclaimer in 
+    the documentation and/or other materials provided with the distribution.
+
+    ** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. 
+    ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+    ** WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 */
 
--- src/bluevgadriver.c
+++ src/bluevgadriver.c
@@ -2,8 +2,7 @@
    BlueVGA library - VGA Driver Library for STM32F103
 
    - This library is intended to work in Arduino IDE using Bluepill STM32F103C8 or STM32F103CB boards
-   - It uses ARDUINO Roger's core for STM32F103C board. Please check it at https://github.com/rogerclarkmelbourne/Arduino_STM32
-     you will find arduino installation for Roger's core at https://github.com/rogerclarkmelbourne/Arduino_STM32/wiki/Installation
+   - It works on both STM32 Core and Roger's core for STM32F103C board.
    - It was tested and runs using the following Arduino Settings for the board:
        Generic STM32F103C series
        Optimize Os (Smallest)
@@ -11,17 +10,17 @@
        CPU Speed(MHz) 72MHz (Normal)
 
     Author Rodrigo Patricio Garcia Corbera (rocorbera@gmail.com)
-    Copyright © 2017-2020 Rodrigo Patricio Garcia Corbera.
+    Copyright © 2017-2020 Rodrigo Patricio Garcia Corbera. 
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
     This code is licensed as Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) - https://creativecommons.org/licenses/by-nc-sa/4.0/
     Redistributions of source code must retain the above copyright notice, and meet al conditions as defined in  https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.
-    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the this disclaimer in
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the this disclaimer in 
     the documentation and/or other materials provided with the distribution.
 
-    ** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+    ** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. 
     ** IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
     ** WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
